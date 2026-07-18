fe22c046e40cdb078c1858dcc8a4d9d13a14119c
alzhwr795-ux <alzhwr795@gmail.com>
2026-07-18 03:05:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print("Hello from Crypto Signals Bot")
+ import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+message = """🚀 إشارة تجريبية
+
+العملة: BTCUSDT
+النوع: LONG
+الدخول: 100000
+الهدف: 101000
+وقف الخسارة: 99500
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Signal sent!")
